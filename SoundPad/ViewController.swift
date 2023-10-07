a1e8bd8a321b8b5ce1b31c39f2ffd1a09d92a9a0
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-10-07 23:02:31 +0300

    add functionality

--- SoundPad/ViewController.swift
+++ SoundPad/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var buttonFour: UIButton!
+    
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonOne.layer.cornerRadius = 8
+        buttonTwo.layer.cornerRadius = 8
+        buttonThree.layer.cornerRadius = 8
+        buttonFour.layer.cornerRadius = 8
+        
     }
-
-
+    
+    @IBAction func soundButton(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.sender.alpha = 0.5
+        sender.alpha = 0.5
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
 }
 
